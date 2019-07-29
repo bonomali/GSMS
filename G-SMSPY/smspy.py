8d2c8d8395a556319b2dcbd0cadae82efaab7afd
Katt Stof <katrinastofferman@autistici.org>
2019-07-29 16:44:05 -0700

    Add files via upload

---
+++ G-SMSPY/smspy.py
@@ -0,0 +1,15 @@
+"""smspy - send sms via python using gmail -
+   Usage: smspy('email', 'password', 'phone_number', 'carrier', 'message'
+   Current Carrier Support: Att, Verizon,"""
+import smtplib
+def smspy(email,password,number,carrier,txt):
+    mailserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    mailserver.ehlo()
+    mailserver.login(email,password)
+    carrier = carrier.lower()
+    if carrier == 'att':
+        number = number + '@text.att.net'
+    if carrier == 'verizon':
+        number = number + '@vtext.com'
+    mailserver.sendmail(email, number, txt)        
+
