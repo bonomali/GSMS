b6db01a85340e283b6e0a7fe4ba5ccec30a82ddd
Katt Stof <katrinastofferman@autistici.org>
2019-07-29 20:52:54 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'GSMS',         
   packages = ['GSMS'],   
-  version = '1.0',      
+  version = '1.2',      
   license='MIT',        
   description = 'send sms using gmail',   
   author = 'KattStof',                   
   author_email = 'Kattstof@autistici.org',      
   url = 'https://github.com/kattstof',   
-  download_url = 'https://github.com/kattstof/G_SMSPY/archive/1.0.tar.gz', 
+  download_url = 'https://github.com/kattstof/G_SMSPY/archive/1.2.tar.gz', 
   keywords = ['SMS', 'Email'],  
   classifiers=[
     'Development Status :: 5 - Stable', 
