be58d75f212d846e50ccabb3a0653af825e34330
Katt Stof <katrinastofferman@autistici.org>
2019-07-29 20:48:37 -0700

    Update GSMS.py

--- GSMS/GSMS.py
+++ GSMS/GSMS.py
@@ -1,8 +1,8 @@
-"""smspy - send sms via python using gmail -
+"""GSMS - send sms via python using gmail -
    Usage:
    From GSMS import GSMS
    GSMS.sms('email', 'password', 'phone_number', 'carrier', 'message'
-   Current Carrier Support: Att, Verizon,"""
+   Current Carrier Support: att, verizon,,tmobile,sprint,virgin,uscellular,boost"""
 import smtplib
 def sms(email,password,number,carrier,txt):
     mailserver = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -13,5 +13,16 @@ def sms(email,password,number,carrier,txt):
         number = number + '@text.att.net'
     if carrier == 'verizon':
         number = number + '@vtext.com'
+    if carrier == 'tmobile':
+        number = number + '@tmomail.net'
+    if carrier == 'sprint':
+        number = number + '@messaging.sprintpcs.com'
+    if carrier == 'virgin':
+        number = number + '@vmobl.com'
+    if carrier == 'uscellular':
+        number = number + '@emil.uscc.net':
+    if carrier == 'boost'
+    number = number + '@myboostmobile.com'
     mailserver.sendmail(email, number, txt)        
+
 
